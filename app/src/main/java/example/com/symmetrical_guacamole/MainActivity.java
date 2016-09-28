010d95114fd1e0736a59bacef26abb75985feddb
Steff May <stefan.may@bizarreware.co.uk>
2016-09-28 12:02:42 +0100

    Initial commit

---
+++ app/src/main/java/example/com/symmetrical_guacamole/MainActivity.java
@@ -0,0 +1,13 @@
+package example.com.symmetrical_guacamole;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
